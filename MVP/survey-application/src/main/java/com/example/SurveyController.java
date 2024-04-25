0ae3b6296b3845383a766170d7d2f1722c4718be
a71na <alina-sophie.maier@iu-study.org>
2024-04-25 09:25:01 +0200

    Add Timons HTML-Code. One can see the new screen with localhost:8080/add-question :-)

--- MVP/survey-application/src/main/java/com/example/SurveyController.java
+++ MVP/survey-application/src/main/java/com/example/SurveyController.java
@@ -16,27 +16,28 @@ public class SurveyController {
 	private final SurveyRepository surveyRepository;
 
 	//GetMapping Methode noch aus dem Technologietest
-	@GetMapping("/survey")
+	/*@GetMapping("/survey")
 	public String getSurveyForm(Model model) {
 		InputForm textInputs = new InputForm();
 		textInputs.getTextInputs().add(new TextInput("Test1"));
 		textInputs.getTextInputs().add(new TextInput("Test2"));
 		model.addAttribute("inputForm", textInputs);
 		return "textinputs";
-	}
+	}*/
 
 	//PostMapping Methode noch aus dem Technologietest
-	@PostMapping("/survey")
+	/*@PostMapping("/survey")
 	@Transactional
 	public String SurveyForm(@ModelAttribute("textinputs") InputForm inputForm, Model model) {
 
 		for(int i = 0; i < inputForm.getTextInputs().size(); i++) {
-			//var umfrage = new Survey(inputForm.getTextInputs().get(i).getContent());
-			//surveyRepository.save(umfrage);
+			var umfrage = new Survey(inputForm.getTextInputs().get(i).getContent());
+			surveyRepository.save(umfrage);
 		}
 		model.addAttribute("inputForm", inputForm);
 		return "result";
-	}
+	}*/
+
 
 	// GetMapping Methode für die Umfrageansicht => Erzeugen des Screens auf localhost:8080
 	@GetMapping("/survey-admin")
@@ -55,6 +56,13 @@ public class SurveyController {
 	public String loadAddSurveyView(Model model) {
 		model.addAttribute("surveyView", new SurveyView());
 		return "addSurvey";
+	}
+
+	//GetMapping Methode für den Screen Frage hinzufügen
+	@GetMapping("/add-question")
+	public String loadAddQuestion(Model model) {
+		model.addAttribute("addQuestion", new SurveyView());
+		return "addQuestion";
 	}
 
 	// Mit einem Klick auf den Speichern-Button leiten wir um zu /survey-save, wo die Daten entsprechend in der
