54221d6bf27d01abf7151da797d2806b993b3e92
a71na <alina-sophie.maier@iu-study.org>
2024-04-28 18:05:09 +0200

    Add sql script for testing

--- MVP/survey-application/src/main/java/com/example/entity/Survey.java
+++ MVP/survey-application/src/main/java/com/example/entity/Survey.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
-import java.util.Date;
+
 
 @Entity
 @Table
---
+++ MVP/survey-application/src/main/resources/data.sql
@@ -0,0 +1,19 @@
+-- This script pre-fills the h2-database with test data at the beginning
+INSERT INTO SURVEY (END_DATE, START_DATE, SURVEY_ID, DESCRIPTION, TITLE)
+VALUES ('2024-06-28', '2024-04-28', 1, 'Das ist eine Testumfrage', 'Testumfrage 1');
+
+INSERT INTO SURVEY (END_DATE, START_DATE, SURVEY_ID, DESCRIPTION, TITLE)
+VALUES ('2024-06-30', '2024-05-03', 2, 'Das ist eine weitere Testumfrage', 'Testumfrage 2');
+
+INSERT INTO QUESTION (QUESTION_ID, SURVEY_ID, QUESTION_TEXT, QUESTION_TYPE, RADIOBUTTON1, RADIOBUTTON2)
+VALUES (1, 1, 'Wie alt bist Du?', 'Radiobutton', '<18', '>=18');
+
+INSERT INTO QUESTION (QUESTION_ID, SURVEY_ID, QUESTION_TEXT, QUESTION_TYPE)
+VALUES (2, 1, 'Wie heißt Du?', 'Textinput');
+
+INSERT INTO QUESTION (QUESTION_ID, SURVEY_ID, QUESTION_TEXT, QUESTION_TYPE, CHECKBOX1, CHECKBOX2, CHECKBOX3)
+VALUES (3, 1, 'Welche Farben sind Deine Lieblingsfarben?', 'Checkbox', 'gelb', 'blau', 'rot');
+
+INSERT INTO QUESTION (QUESTION_ID, SURVEY_ID, QUESTION_TEXT, QUESTION_TYPE, CHECKBOX1, CHECKBOX2, CHECKBOX3, CHECKBOX4)
+VALUES (4, 2, 'Wie lautet Deine Lieblingsprogrammiersprache?', 'Checkbox', 'Java', 'C', 'OCaml', 'C++');
+
