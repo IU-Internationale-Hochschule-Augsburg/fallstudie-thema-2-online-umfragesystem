68af6bd7768ac1c5ad248bdb0c8213cd381c986a
a71na <alina-sophie.maier@iu-study.org>
2024-05-09 23:02:20 +0200

    added cute error page °._.°

---
+++ MVP/survey-application/src/main/java/com/example/SurveyErrorController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SurveyErrorController implements ErrorController {
+    private final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String loadErrorPage() {
+        return "error";
+    }
+}
