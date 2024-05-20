92c998209b4d8c305d07e6115d177c102c0a3e9c
a71na <alina-sophie.maier@iu-study.org>
2024-05-20 11:51:56 +0200

    small changes

--- MVP/survey-application/src/main/java/com/surveymaster/ParticipantSurveyController.java
+++ MVP/survey-application/src/main/java/com/surveymaster/ParticipantSurveyController.java
@@ -1,16 +1,25 @@
 package com.surveymaster;
 
+import com.surveymaster.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 
 public class ParticipantSurveyController {
+    QuestionRepository questionRepository;
+
     @GetMapping("/participant-view")
     public String loadParticipantView(Model model) {
         return "respondentCheckBoxView";
+    }
+
+    @PostMapping("/save-response")
+    public String saveResponse(Model model) {
+        return "redirect:/participant-view";
     }
 }
