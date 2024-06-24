7b091625a1ba79e9a3ceaec924d51a070846d629
a71na <alina-sophie.maier@iu-study.org>
2024-06-24 23:17:23 +0200

    connected user settings #159 First developer tests are done - there are no obvious errors (explorative testing)

--- MVP/survey-application/src/main/java/com/surveymaster/LoginController.java
+++ MVP/survey-application/src/main/java/com/surveymaster/LoginController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Objects;
+
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
@@ -36,16 +38,21 @@ public class LoginController {
         var user = userRepository.findByUsername(registerForm.getUsername());
         var email = userRepository.findByEmail(registerForm.getEmail());
 
+        // check if the email and username are not yet registered
         if (user == null && email == null) {
+            // set new dates from the user
             User newUser = new User();
             newUser.setUsername(registerForm.getUsername());
             newUser.setFirstname(registerForm.getFirstname());
             newUser.setSurname(registerForm.getSurname());
             newUser.setEmail(registerForm.getEmail());
+
+            // encryption + check if password and confirm-password are the same
             var encryptedPassword = BCrypt.hashpw(registerForm.getPassword(), BCrypt.gensalt());
             if (BCrypt.checkpw(registerForm.getConfirmPassword(), encryptedPassword)) {
                 newUser.setPassword(encryptedPassword);
             } else {
+                // error handling, if the passwords are not the same
                 model.addAttribute(registerForm);
                 model.addAttribute("errorMessage", "Die Passwörter stimmen nicht überein!");
                 return "filledRegisterScreen";
@@ -54,6 +61,7 @@ public class LoginController {
             userRepository.save(newUser);
             model.addAttribute(newUser);
         } else {
+            // error handling, if username or e-mail are already registered
             model.addAttribute(registerForm);
             model.addAttribute("errorMessage", "Benutzername oder E-Mail ist schon vergeben!");
             return "filledRegisterScreen";
@@ -69,10 +77,10 @@ public class LoginController {
 
     @GetMapping("/user-actions")
     public String handleUserActions(@RequestParam("userActions") String userActions, Model model) {
-        if(userActions.startsWith("logout,")) {
+        if (userActions.startsWith("logout,")) {
             model.addAttribute(userActions);
             return "redirect:/logout";
-        } else if(userActions.startsWith("settings,")) {
+        } else if (userActions.startsWith("settings,")) {
             var currUser = surveyService.getCurrentUser();
             UserSettingsForm userSettingsForm = new UserSettingsForm();
 
@@ -93,32 +101,51 @@ public class LoginController {
 
     @PostMapping("/save-user-settings")
     public String saveUserSettings(Model model, @ModelAttribute("userSettingsForm") UserSettingsForm userSettingsForm) {
-        var user = userSettingsForm.getUsername();
         var currentUser = userRepository.findByUsername(userSettingsForm.getUsername());
 
         // emails are matching
-        if(currentUser.getEmail().equalsIgnoreCase(userSettingsForm.getEmail())) {
+        if (currentUser.getEmail().equalsIgnoreCase(userSettingsForm.getEmail())) {
+            // set new user-data
             currentUser.setFirstname(userSettingsForm.getFirstname());
             currentUser.setSurname(userSettingsForm.getSurname());
         } else {
             // emails are not matching => check, if the new email is already registered
             var currentUserByEmail = userRepository.findByEmail(userSettingsForm.getEmail());
-            if(currentUserByEmail == null) {
+            if (currentUserByEmail == null) {
+                // if e-mail is not yet registered, set new user-data
                 currentUser.setFirstname(userSettingsForm.getFirstname());
                 currentUser.setSurname(userSettingsForm.getSurname());
                 currentUser.setEmail(userSettingsForm.getEmail());
             } else {
-                // TODO: Error-Message for taken email address
-                model.addAttribute("errorMessage", "");
+                // error handling if e-mail is already registered
+                model.addAttribute("errorMessage", "Die E-Mail Adresse ist bereits vergeben!");
                 return "userSettings";
             }
         }
 
-        // TODO: compare passwords and encrypt new password
-        /*model.addAttribute("errorMessage", "");
-        return "userSettings";*/
+        // Compare and check passwords, if user typed new passwords
+        if (!Objects.equals(userSettingsForm.getPassword(), "") && !Objects.equals(userSettingsForm.getConfirmPassword(), "")) {
+            var encryptedPassword = BCrypt.hashpw(userSettingsForm.getPassword(), BCrypt.gensalt());
 
+            // check, if the old password is the same as in the database
+            if (BCrypt.checkpw(userSettingsForm.getOldPassword(), currentUser.getPassword())) {
+                // check, if the new password and the confirm-password are the same
+                if (BCrypt.checkpw(userSettingsForm.getConfirmPassword(), encryptedPassword)) {
+                    currentUser.setPassword(encryptedPassword);
+                } else {
+                    // error handling, if the passwords are not the same
+                    model.addAttribute("errorMessage", "Die neuen Passwörter stimmen nicht überein!");
+                    return "userSettings";
+                }
+            } else {
+                // error handling, if the old password is not the same as in the database
+                model.addAttribute("errorMessage", "Das alte Passwort stimmt nicht mit dem aus der Datenbank überein!");
+                return "userSettings";
+            }
+        }
+
+        // save changes
+        userRepository.save(currentUser);
         return "redirect:/survey-admin";
-
     }
 }
