12f4949171f123693f3713e340d9ff05c1e208a1
d00mfry <leon.hehl@iu-study.org>
2024-06-25 21:08:36 +0200

    First unit tests for LoginController #149;

--- MVP/survey-application/src/main/java/com/surveymaster/repository/AnswerRepository.java
+++ MVP/survey-application/src/main/java/com/surveymaster/repository/AnswerRepository.java
@@ -4,7 +4,11 @@ import com.surveymaster.entity.Answer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnswerRepository extends CrudRepository<Answer, Long> {
     void deleteByQuestionId(Long questionId);
+
+    List<Answer> findByQuestionId(Long questionId);
 }
